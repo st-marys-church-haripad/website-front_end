26671b9efcdee25235f77abd75e10c9870b315d0
jerineaso <jerin.regi@skizzle.tech>
2025-08-31 23:25:08 +0530

    live stream

--- src/pages/Live.jsx
+++ src/pages/Live.jsx
@@ -1,10 +1,21 @@
-import NoData from '@/components/shared/NoData'
 import React from 'react'
+import ReactPlayer from 'react-player';
 
 const Live = () => {
   return (
-    <div className='container mt-3'>
-        <NoData />
+    <div className='container'>
+      <div id="day_1" className='mt-3'>
+        <h3 className='fs-16'>Day 1 - 1st September 2025</h3>
+        <hr />
+        <p className='fs-14'>
+          Chief Celebrant : <b>H.G.DR. Yuhanon Mar Demetrios</b>
+          <br />
+          Live from : <b>7:30 am</b>
+        </p>
+      </div>
+      <div className='mb-2'>
+        <ReactPlayer src="https://www.youtube.com/live/sJCJ1nWynA8?si=tNkOXHyd30nULncO" width={'100%'} height={'100%'} autoPlay controls></ReactPlayer>
+      </div>
     </div>
   )
 }
