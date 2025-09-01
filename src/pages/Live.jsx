1b65bcb11e98a1f3e13db05c20379c8235582981
jerineaso <jerin.regi@skizzle.tech>
2025-09-01 23:28:34 +0530

    live update

--- src/pages/Live.jsx
+++ src/pages/Live.jsx
@@ -5,16 +5,16 @@ const Live = () => {
   return (
     <div className='container'>
       <div id="day_1" className='mt-3'>
-        <h3 className='fs-16'>Day 1 - 1st September 2025</h3>
+        <h3 className='fs-16'>Day 2 - 2nd September 2025</h3>
         <hr />
         <p className='fs-14'>
-          Chief Celebrant : <b>H.G.DR. Yuhanon Mar Demetrios</b>
+          Chief Celebrant : <b className='malayalam-font'>അഭി.ഡോ. എബ്രഹാം മാർ സെറാഫിം</b>
           <br />
           Live from : <b>7:30 am</b>
         </p>
       </div>
       <div className='mb-2'>
-        <ReactPlayer src="https://www.youtube.com/live/sJCJ1nWynA8?si=tNkOXHyd30nULncO" width={'100%'} height={'100%'} autoPlay controls></ReactPlayer>
+        <ReactPlayer src="https://www.youtube.com/live/YFXaMxYhEIE?si=tjgU2UeNxFBqOFSK" width={'100%'} height={'100%'} autoPlay controls></ReactPlayer>
       </div>
     </div>
   )
